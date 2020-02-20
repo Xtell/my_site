bda95677437c95a6ee2a86e1188a24c61f86d09a
xtell <evstafyevan96@gmail.com>
2020-02-20 15:37:55 +0500

    Фикс

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ var phoneButton = document.querySelector(".main-footer__phone-btn");
 phoneButton.addEventListener('click', function(evt) {
     evt.preventDefault();
     phoneButton.classList.add('main-footer__phone-btn--show');
-    phoneButton.innerHTML = '+7 908 8243711';
+    phoneButton.innerHTML = '+7 (908) 824-37-11';
     phoneButton.setAttribute('href', 'tel:+79088243711');
 });
